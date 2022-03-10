677171d957817b4811a7d09de78e8df6e36d0e91
Raghavendra K R <raghavendra@sodaru.com>
2022-03-10 16:50:45 +0530

    improved SimpleMenuWithNextLinks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,48 @@
 import { Box, Typography } from "@mui/material";
+import { useRouter } from "next/router";
 import { FunctionComponent } from "react";
 import { Layout } from "../src/Layout";
 import { SimpleMenuWithNextLinks } from "../src/SimpleMenuWithNextLinks";
 import { SodaruPageComponentType } from "../src/SodaruApp";
 
-export const ComposedLayout: FunctionComponent = ({ children }) => {
-  return (
-    <Layout
-      menu={
-        <SimpleMenuWithNextLinks
-          pages={[
-            { link: "/", label: "Home" },
-            { link: "sodaru-theme", label: "Sodaru Theme" },
-            { link: "sodaru-logo", label: "Sodaru Logo" }
-          ]}
-        />
+const Menu: FunctionComponent<{ route: string }> = ({ route }) => (
+  <SimpleMenuWithNextLinks
+    pages={[
+      { link: "/", label: "Home", menuItemProps: { selected: route === "/" } },
+      {
+        link: "sodaru-theme",
+        label: "Sodaru Theme",
+        menuItemProps: { selected: route === "/sodaru-theme" }
+      },
+      {
+        link: "sodaru-logo",
+        label: "Sodaru Logo",
+        menuItemProps: { selected: route === "/sodaru-logo" }
       }
-    >
-      {children}
-    </Layout>
-  );
+    ]}
+  />
+);
+
+export const ComposedLayout: FunctionComponent = ({ children }) => {
+  const router = useRouter();
+  return <Layout menu={<Menu route={router.route} />}>{children}</Layout>;
 };
 
 const Index: SodaruPageComponentType = () => {
+  const router = useRouter();
   return (
     <Box>
-      <Typography variant="h2">Welcome</Typography>
-      <Typography variant="h3" fontFamily="sans-serif">
+      <Typography variant="h3">Welcome</Typography>
+      <Typography variant="h4">
         This is a showcase of Common UI Components
       </Typography>
+      <Typography variant="body1">This page does not have a Layout</Typography>
+      <Typography variant="body2">
+        All Demo Pages include a Layout with Menu
+      </Typography>
+      <Box width={200}>{<Menu route={router.route} />}</Box>
     </Box>
   );
 };
-
-Index.layout = ComposedLayout;
 
 export default Index;
--- src/SimpleMenuWithNextLinks.tsx
+++ src/SimpleMenuWithNextLinks.tsx
@@ -1,18 +1,23 @@
-import { MenuList, MenuItem, ListItemText } from "@mui/material";
+import { MenuList, MenuItem, ListItemText, MenuItemProps } from "@mui/material";
 import Link from "next/link";
 import { FunctionComponent, ReactElement } from "react";
 
 export const SimpleMenuWithNextLinks: FunctionComponent<{
-  pages: (string | { label: string; link: string })[];
+  pages: (
+    | string
+    | { label: string; link: string; menuItemProps?: MenuItemProps }
+  )[];
 }> = ({ pages }) => {
   const pageLinks: ReactElement[] = [];
   for (const page of pages) {
     const link = typeof page == "string" ? page : page.link;
     const label = typeof page == "string" ? page : page.label;
+    const menuItemProps =
+      typeof page == "string" ? {} : page.menuItemProps || {};
     pageLinks.push(
       // eslint-disable-next-line @next/next/link-passhref
       <Link href={link} key={link}>
-        <MenuItem>
+        <MenuItem {...menuItemProps}>
           <ListItemText>{label}</ListItemText>
         </MenuItem>
       </Link>
