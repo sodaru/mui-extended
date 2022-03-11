9309e9f17bd58b029be9d00e621577cd8bf86d34
Raghavendra K R <raghavendra@sodaru.com>
2022-03-11 21:01:33 +0530

    hard coded alt in sodaru logo

--- src/SodaruLogo.tsx
+++ src/SodaruLogo.tsx
@@ -3,14 +3,14 @@ import LogoImage from "./assets/logo.png";
 import { SodaruImage, SodaruImageProps } from "./SodaruImage";
 
 export const SodaruLogo: FunctionComponent<
-  Omit<SodaruImageProps, "src">
+  Omit<SodaruImageProps, "src" | "alt">
 > = props => {
   return (
     <SodaruImage
       src={LogoImage}
-      alt="Sodaru Logo"
       width={100}
       height={100}
+      alt="Sodaru Logo"
       {...props}
     />
   );
